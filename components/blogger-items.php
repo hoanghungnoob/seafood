e69376cee3271529c66979b91af4c1091f55519c
Van Thao <levanthao@gmail.com>
2023-12-20 16:23:08 +0700

    Change blogger

--- components/blogger-items.php
+++ components/blogger-items.php
@@ -7,33 +7,8 @@
 <div class="card" style="width: 18rem;">
   <img src="../root/assets/images-blogger/photo-bg1.png" class="card-img-top" alt="images">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <label class="card-title fs-6">Card title</label>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
-<div class="card" style="width: 18rem;">
-  <img src="../root/assets/images-blogger/photo-bg1.png" class="card-img-top" alt="images">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="../root/assets/images-blogger/photo-bg1.png" class="card-img-top" alt="images">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="../root/assets/images-blogger/photo-bg1.png" class="card-img-top" alt="images">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
